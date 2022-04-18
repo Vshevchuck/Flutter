966cbfb36c63e5d3e847d980dd3ecccd83c76360
Vshevchuck <vlad3032002@gmail.com>
2022-04-18 03:12:25 +0200

    add menu with appbar, texts and placeholder in coloumn

--- lessonsFlutterTests/flutter_app3/lib/widgets/user_profile.dart
+++ lessonsFlutterTests/flutter_app3/lib/widgets/user_profile.dart
@@ -6,10 +6,27 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Налаштування'),
+        backgroundColor: Colors.lightGreen,
+      ),
       body: Container(
         color: Colors.lightGreenAccent,
-        child: Center(
-          child: Text('test'),
+        //maximum allowable width
+        width: double.infinity,
+        child: Column(
+          //column in center
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              child: Placeholder(),
+            ),
+            Text('Шевчук Владислав'),
+            Text('+380688110424'),
+            Text('vlad_shepa'),
+          ],
         ),
       ),
     );
