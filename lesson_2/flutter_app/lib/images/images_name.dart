69b8047e51c79194cffadf684042534ec9d98943
Vshevchuck <vlad3032002@gmail.com>
2022-05-01 01:05:56 +0200

    Create header

---
+++ lesson_2/flutter_app/lib/images/images_name.dart
@@ -0,0 +1,9 @@
+abstract class ImagesName{
+  static const logo = 'assets/image/staff_logo.png';
+  static const product1 = 'assets/image/product1.jpeg';
+  static const product2 = 'assets/image/product2.jpeg';
+  static const product3 = 'assets/image/product3.jpeg';
+  static const product4 = 'assets/image/product4.jpeg';
+  static const product5 = 'assets/image/product5.jpeg';
+  static const product6 = 'assets/image/product6.jpeg';
+}
--- lesson_2/flutter_app/lib/widgets/main_page.dart
+++ lesson_2/flutter_app/lib/widgets/main_page.dart
@@ -1,31 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_app3/images/images_name.dart';
 
 class MainPage extends StatelessWidget {
-
   MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.white,
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Center',
-            ),
-          )),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                width: double.infinity,
+                height: 110,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: const Image(
+                        width: 120,
+                        height: 90,
+                        image: AssetImage(ImagesName.logo),
+                      ),
+                    ),
+                    Positioned(
+                      right: 10,
+                      bottom: 0,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: const [
+                              Text('0',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.w500)),
+                              Icon(Icons.add_shopping_cart_outlined,
+                                  color: Colors.black),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      left: 10,
+                      bottom: 1,
+                      child: Row(
+                        children: [
+                          Text('Total amount - ',
+                              style: TextStyle(fontWeight: FontWeight.w500)),
+                          Text('0',
+                              style: TextStyle(fontWeight: FontWeight.w500))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top : 15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      child: Text('hoody',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 17))),
+                  Container(
+                      child: Text('jeans',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 17))),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
